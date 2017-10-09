4b2383e9017deebd55d9dcafa53aa71d1ffd373d
nicole <nicolesteffane@gmail.com>
2017-10-09 11:24:02 -0300

    esqueci de trocar static_url para static_root

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICL_URL = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
